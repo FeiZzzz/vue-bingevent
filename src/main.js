c0886cd7b11e2fb7b83b4cb7c548a5ff0414cbe1
FeiZzzz <1552508702@qq.com>
2022-06-26 20:25:01 +0800

    初始化axios和eleme

--- src/main.js
+++ src/main.js
@@ -2,9 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import axios from 'axios'
 Vue.config.productionTip = false
-
+// 设置全局基地址
+axios.defaults.baseURL = 'http://big-event-vue-api-t.itheima.net'
+// 把axios 挂载到 Vue原型对象上
+Vue.prototype.$http = axios
+// 挂载elme
+Vue.use(ElementUI)
 new Vue({
   router,
   store,
